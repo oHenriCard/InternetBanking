a7a6b440df4fc88da52f1dbd973b43adb327d241
Henri Card <111603780+oHenriCard@users.noreply.github.com>
2025-07-30 10:47:45 -0300

    Withdraw rename

---
+++ backend/src/main/java/com/finalproject/internet/banking/internetbanking/dtos/Withdraw.java
@@ -0,0 +1,31 @@
+package com.finalproject.internet.banking.internetbanking.dtos;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class Withdraw {
+    @NotBlank(message = "O número da conta é obrigatória!")
+    private String accountNum;
+
+    @NotNull(message = "O valor é obrigatório!")
+    private BigDecimal value;
+
+// GETTERS N SETTERS
+    public String getAccountNum() {
+        return this.accountNum;
+    }
+    public void setAccountNum(String accountNum) {
+        this.accountNum = accountNum;
+    }
+
+    public BigDecimal getValue() {
+        return this.value;
+    }
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+
+}
