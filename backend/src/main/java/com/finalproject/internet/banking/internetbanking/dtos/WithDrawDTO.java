2ab227a670d8e72b1b4a12cdb0a452e70d1a6942
Henri Card <111603780+oHenriCard@users.noreply.github.com>
2025-07-30 10:47:45 -0300

    WithdrawDTO rename*

--- backend/src/main/java/com/finalproject/internet/banking/internetbanking/dtos/WithDrawDTO.java
+++ backend/src/main/java/com/finalproject/internet/banking/internetbanking/dtos/WithDrawDTO.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public class WithdrawDTO {
+public class WithdrawDTO{
     @NotBlank(message = "O número da conta é obrigatória!")
     private String accountNum;
 
