61ebd8c00498ddb4911d64ffd6aa1743a8557ea1
Henri Card <111603780+oHenriCard@users.noreply.github.com>
2025-08-03 12:23:56 -0300

    remove restConfig from Main

--- backend/src/main/java/com/finalproject/internet/banking/internetbanking/InternetbankingApplication.java
+++ backend/src/main/java/com/finalproject/internet/banking/internetbanking/InternetbankingApplication.java
@@ -2,17 +2,11 @@ package com.finalproject.internet.banking.internetbanking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class InternetbankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InternetbankingApplication.class, args);
-	}
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
 	}
 }
